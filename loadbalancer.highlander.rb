8e97284df36aba332b0f17189d6fc726bdf6e5e8
Aaron Walker <aaronpwalker@gmail.com>
2018-06-01 15:32:35 +1000

    use mapping params for dns and ssl cert

--- loadbalancer.highlander.rb
+++ loadbalancer.highlander.rb
@@ -5,8 +5,14 @@ HighlanderComponent do
     StackParam 'EnvironmentName', 'dev', isGlobal: true
     StackParam 'EnvironmentType', 'development', isGlobal: true
     StackParam 'StackOctet', isGlobal: true
-    ComponentParam 'DnsDomain' if defined?(records)
-    ComponentParam 'SslCertId'
+    MappingParam('DnsDomain') do
+      map 'AccountId'
+      attribute 'DnsDomain'
+    end
+    MappingParam('SslCertId') do
+      map 'AccountId'
+      attribute 'SslCertId'
+    end
     subnet_parameters({'public'=>{'name'=>'Public'}}, maximum_availability_zones)
     subnet_parameters({'compute'=>{'name'=>'Compute'}}, maximum_availability_zones) if defined?(loadbalancer_scheme) && loadbalancer_scheme == 'internal'
     OutputParam component: 'vpc', name: "VPCId"
